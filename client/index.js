2dd2ba39cab8f4178ac75d8709927302d5fb4306
Cassie Rosner <cerosner@gmail.com>
2019-03-10 18:59:42 -0400

    feat: refactorsclient entry point with router

--- client/index.js
+++ client/index.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import { Router } from 'react-router-dom'
+import history from './history'
 import store from './store'
+import App from './app'
 
 ReactDOM.render(
   <Provider store={store}>
-    <div>Hello, world!</div>
+    <Router history={history}>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById('app')
 )
